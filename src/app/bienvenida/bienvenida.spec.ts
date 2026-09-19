bcb2d02ffd76f095241495e499e917bf601f93bf
sebasmedina267 <sebasmedina267@gmail.com>
2026-09-19 20:31:21 +0200

    Bienvenida

---
+++ src/app/bienvenida/bienvenida.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Bienvenida } from './bienvenida';
+
+describe('Bienvenida', () => {
+  let component: Bienvenida;
+  let fixture: ComponentFixture<Bienvenida>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Bienvenida],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Bienvenida);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/bienvenida/bienvenida.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-bienvenida',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './bienvenida.html',
+  styleUrls: ['./bienvenida.css']
+})
+export class Bienvenida {
+  nombre = '';
+  mensaje = '';
+
+  onSubmit() {
+    this.mensaje = `Hola ${this.nombre}, bienvenido a la aplicación.`;
+  }
+}
